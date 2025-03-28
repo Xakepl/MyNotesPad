cfc93767d8adf16c317b3d2fa912bb62e0063c5e
200km\h ishak na gazu <lucoroalava@gmail.com>
2025-03-28 14:08:49 +0300

    коммит

--- app/src/main/java/com/missdark/mynotespad/MainActivity.java
+++ app/src/main/java/com/missdark/mynotespad/MainActivity.java
@@ -1,19 +1,79 @@
 package com.missdark.mynotespad;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton create;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        create = findViewById(R.id.create);
+        create.setOnClickListener(v -> {
+            // Создаем EditText
+            final EditText inputEditText = new EditText(this);
+            inputEditText.setHint("Введите текст");
+            inputEditText.setPadding(32, 32, 32, 32);
 
+// Создаем AlertDialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Ввод данных")
+                    .setMessage("Пожалуйста, введите название заметки:")
+                    .setView(inputEditText)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String inputText = inputEditText.getText().toString();
+                            if (!inputText.isEmpty()) {
+                               Intent intent = new Intent(MainActivity.this, editor.class);
+                                File file = new File(getFilesDir(), (String)inputText +".txt");
+                               startActivity(intent);
+                               new Thread(() -> {
+                                   try{
+                                       FileOutputStream fos = new FileOutputStream(file);
+                                       intent.putExtra("FILE_PATH", file.getAbsolutePath());
+
+                                   } catch (FileNotFoundException e) {
+                                       Toast.makeText(MainActivity.this,"Файл не был создан" + e.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                                       throw new RuntimeException(e);
+                                   }
+
+                               }).start();
+                            }
+                        }
+                    })
+                    .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        });
     }
+
+
 }
--- app/src/main/java/com/missdark/mynotespad/editor.java
+++ app/src/main/java/com/missdark/mynotespad/editor.java
@@ -2,6 +2,10 @@ package com.missdark.mynotespad;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -13,19 +17,48 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class editor extends AppCompatActivity {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 
-    FloatingActionButton create;
+public class editor extends AppCompatActivity {
+    EditText title;
+    String strTitle;
+    EditText text;
+    ImageView save;
+    String strText;
+
+    String fileS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-        create.findViewById(R.id.create);
-        create.setOnClickListener(v -> {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Название заметки: ").setView(R.layout.dialog).create().show();
+        fileS = (String) getIntent().getSerializableExtra("FILE");
+        File file = new File(fileS);
+
+        FileOutputStream fos;
+
+        save = findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    fos = new FileOutputStream(file, true);
+                    fos.write(strTitle.getBytes(StandardCharsets.UTF_8));
+                    fos.write(strText.getBytes(StandardCharsets.UTF_8)); // Указываем кодировку
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
         });
+
+        //FileOnputStream fos = new FileInputStream();
+
 
     }
 }
