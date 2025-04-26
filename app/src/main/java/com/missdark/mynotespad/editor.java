a9e4c612515a4c75904388dcfadc0316eaecd0af
Xakepl <lucoroalava@gmail.com>
2025-04-26 15:29:43 +0300

    IT_CUBE 26_04_25

--- app/src/main/java/com/missdark/mynotespad/editor.java
+++ app/src/main/java/com/missdark/mynotespad/editor.java
@@ -1,7 +1,11 @@
 package com.missdark.mynotespad;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.AbsoluteSizeSpan;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
@@ -34,90 +38,112 @@ public class editor extends AppCompatActivity implements Serializable {
     Spinner FSspinner;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_editor);
-        title = findViewById(R.id.titleText);
-        text = findViewById(R.id.textC);
-        save = findViewById(R.id.save);
-        save.setOnClickListener(v -> save());
-        back = findViewById(R.id.back);
-        clear = findViewById(R.id.clear);
-        FSspinner = findViewById(R.id.font_size);
-        back.setOnClickListener(v -> {
-            save();
-            Intent i = new Intent(editor.this, MainActivity.class);
-            startActivity(i);
-        });
-        clear.setOnClickListener(v -> clear());
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_editor);
+            title = findViewById(R.id.titleText);
+            text = findViewById(R.id.textC);
+            save = findViewById(R.id.save);
+            save.setOnClickListener(v -> save());
+            back = findViewById(R.id.back);
+            clear = findViewById(R.id.clear);
+            FSspinner = findViewById(R.id.font_size);
 
-        if (getIntent().getSerializableExtra("STATE") == MainActivity.Status.OPENFILEANDEDIT) {
-            try {
-                openAndEdit();
-            } catch (FileNotFoundException e) {
-                Log.e("unsucces openFile", String.valueOf(e));
-                throw new RuntimeException(e);
+            back.setOnClickListener(v -> {
+                save();
+                Intent i = new Intent(editor.this, MainActivity.class);
+                startActivity(i);
+            });
+            clear.setOnClickListener(v -> clear());
+
+            if (getIntent().getSerializableExtra("STATE") == MainActivity.Status.OPENFILEANDEDIT) {
+                try {
+                    openAndEdit();
+                } catch (FileNotFoundException e) {
+                    Log.e("unsucces openFile", String.valueOf(e));
+                    throw new RuntimeException(e);
+                }
             }
+
+            title.setOnClickListener(v -> {
+                FSspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                        switch (FSspinner.getSelectedItem().toString()) {
+                            case "8":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8);break;
+                            case "9":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 9);break;
+                            case "10":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);break;
+                            case "11":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11);break;
+                            case "12":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);break;
+                            case "14":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);break;
+                            case "16":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);break;
+                            case "18":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);break;
+                            case "20":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);break;
+                            case "22":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);break;
+                            case "24":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);break;
+                            case "26":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);break;
+                            case "28":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);break;
+                            case "36":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36);break;
+                            case "48":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);break;
+                            case "72":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 72);break;
+                            case "84":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 84);break;
+                            case "96":title.setTextSize(TypedValue.COMPLEX_UNIT_SP, 96);break;
+                        }
+                        //TODO Сделать пандинги для заголовка
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {}
+                });
+            });
+            text.setOnClickListener(v -> {
+                FSspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                        // Получаем выделенный текст
+//                        int start = editText.getSelectionStart();
+//                        int end = editText.getSelectionEnd();
+//
+//                        if (start != end) { // Если есть выделение
+//                            SpannableString spannable = new SpannableString(text.getText());
+//
+//                            // Устанавливаем новый размер (в SP)
+//                            spannable.setSpan(
+//                                    new AbsoluteSizeSpan(16, true), // 16sp (true = в SP)
+//                                    start,
+//                                    end,
+//                                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+//                            );
+
+                            text.setText(spannable);
+                        }
+                        switch (FSspinner.getSelectedItem().toString()) {
+                            case "8":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8);break;
+                            case "9":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 9);break;
+                            case "10":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);break;
+                            case "11":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11);break;
+                            case "12":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);break;
+                            case "14":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);break;
+                            case "16":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);break;
+                            case "18":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);break;
+                            case "20":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);break;
+                            case "22":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);break;
+                            case "24":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);break;
+                            case "26":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);break;
+                            case "28":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);break;
+                            case "36":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36);break;
+                            case "48":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);break;
+                            case "72":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 72);break;
+                            case "84":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 84);break;
+                            case "96":text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 96);break;
+                        }
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+            });
+
         }
-        FSspinner.setOnItemClickListener((parent, view, position, id) -> {
-            switch (FSspinner.getSelectedItem().toString()) {
-                case "8":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8);
-                    break;
-                case "9":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 9);
-                    break;
-                case "10":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10);
-                    break;
-                case "11":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 11);
-                    break;
-                case "12":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
-                    break;
-                case "14":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
-                    break;
-                case "16":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
-                    break;
-                case "18":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
-                    break;
-                case "20":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
-                    break;
-                case "22":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 22);
-                    break;
-                case "24":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
-                    break;
-                case "26":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26);
-                    break;
-                case "28":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);
-                    break;
-                case "36":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 36);
-                    break;
-                case "48":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 48);
-                    break;
-                case "72":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 72);
-                    break;
-                case "84":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 84);
-                    break;
-                case "96":
-                    text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 96);
-                    break;
-            }
-        });
-    }
 
     void openAndEdit() throws FileNotFoundException {
         file = (File) getIntent().getSerializableExtra("FILE");
@@ -165,7 +191,7 @@ public class editor extends AppCompatActivity implements Serializable {
 
     void clear(){
         title.setText("");
-        text.setText("  ");
+        text.setText("");
     }
 
 
